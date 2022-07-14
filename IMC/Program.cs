e47258768c9fcc54d26e2375f2ce86f2ac9ac323
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-07-14 12:16:21 -0300

    Adicionado para ler nome,idade

--- IMC/Program.cs
+++ IMC/Program.cs
@@ -15,13 +15,22 @@ namespace IMC
             Console.WriteLine("Digite sua altura: ");
             float altura = (float)Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("Qual é a sua idade? ");
+            string idade = Convert.ToString(Console.ReadLine());
+
+
+            Console.WriteLine("Seu nome é " + nome_);
+            Console.WriteLine("Seu peso é " + peso);
+            Console.WriteLine("Sua altura é " + altura);
+            Console.WriteLine("Sua idade é " + idade);
+
             float imc = peso / (altura * altura);
 
-            if (imc > 18.5)
+            if (imc <= 18.5)
             {
                 Console.WriteLine("IMC abaixo");
             }
-            else if (imc <= 24.9)
+            else if (imc <= 24.9 && imc > 18.5)
             {
                 Console.WriteLine("Adequado");
             }
