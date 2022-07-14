170e48a6cae6f36653c1dc325e7ed48aa6db4b3e
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-07-14 11:50:00 -0300

    Exerício IMC

---
+++ IMC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome? ");
+            string nome_= Convert.ToString(Console.ReadLine());
+                
+            Console.WriteLine("Digite seu peso: ");
+            float peso = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+
+            float imc = peso / (altura * altura);
+
+            if (imc > 18.5)
+            {
+                Console.WriteLine("IMC abaixo");
+            }
+            else if (imc <= 24.9)
+            {
+                Console.WriteLine("Adequado");
+            }
+            else if (imc > 25)
+            {
+                Console.WriteLine("IMC acima");
+            }
+
+
+
+
+        }
+    }
+}
